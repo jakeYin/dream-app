96d8b14337bc7a145f238212048467ecc17d3576
jake <jake@126.com>
2019-06-08 08:32:45 +0800

    add test case

--- app/src/test/java/cn/sddman/download/ExampleUnitTest.java
+++ app/src/test/java/cn/sddman/download/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package cn.sddman.download;
 
+import com.google.gson.Gson;
+
 import org.junit.Test;
+
+import cn.sddman.bt.spider.MagnetFetchDyttDetailParserImp;
+import cn.sddman.download.mvp.e.MagnetRule;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,32 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test2() {
+        try {
+            String ruleStr = "{\"site\": \"电影天堂-Movie\",\n" +
+                    "    \"url\": \"http://www.ygdy8.net\",\n" +
+                    "    \"parserClass\": \"cn.sddman.bt.spider.MagnetFetchDyttImp\",\n" +
+                    "    \"parserDetailClass\":\"cn.sddman.bt.spider.MagnetFetchDyttDetailParserImp\",\n" +
+                    "    \"list\": \"//*[@id='header']/div/div[3]/div[3]/div[2]/div[2]/div[2]/ul/table\",\n" +
+                    "    \"href\": \".//tbody/tr[2]/td[2]/b/a//@href\",\n" +
+                    "    \"name\": \".//tbody/tr[1]/td[2]/b/a\",\n" +
+                    "    \"desc\": \".//tbody/tr[2]\",\n" +
+                    "    \"detailBaseUrl\":\"http://www.ygdy8.com\",\n" +
+                    "    \"detailUrl\": \".//tbody/tr[2]\",\n" +
+                    "    \"detailFtpLinks\":\"//*[@id='Zoom']//a\",\n" +
+                    "    \"detailMagnetLinks\":\"//*[@id='Zoom']/span/p[1]/a\",\n" +
+                    "    \"source\": \"http://s.ygdy8.com/plus/so.php?keyword=%1$s&searchtype=titlekeyword&channeltype=0&orderby=&kwtype=0&pagesize=10&typeid=1&PageNo=%2$s\"}";
+            Gson gson = new Gson();
+//*[@id="Zoom"]/span/table[2]/tbody/tr/td/a
+
+            MagnetRule rule = gson.fromJson(ruleStr, MagnetRule.class);
+            MagnetFetchDyttDetailParserImp imp = new MagnetFetchDyttDetailParserImp();
+            imp.parser(rule,"https://www.ygdy8.com/html/gndy/dyzz/20190308/58314.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
